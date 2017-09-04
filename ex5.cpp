11bda57c49f8cfa7cfbc9223b4b7ad2a432c92b9
Paul DePalma <depalma@gonzaga.edu>
2017-09-03 22:56:58 -0700

    ex5.cpp

---
+++ ex5.cpp
@@ -0,0 +1,21 @@
+/*
+CPSC 121-0X
+Paul De Palma
+depalma
+Example 5 
+*/
+
+//all data types have sizes and you can check them 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "The size of int is " << sizeof(int) << " bytes" << endl; 
+ cout << "The size of long int is " << sizeof(long int) << " bytes" << endl; 
+ cout << "The size of char is " << sizeof(char) << " bytes" << endl; 
+ cout << "The size of float is " << sizeof(float) << " bytes" << endl; 
+ cout << "The size of double is " << sizeof(double) << " bytes" << endl; 
+ return 0;
+}
