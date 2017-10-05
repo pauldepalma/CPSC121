4345902c232ea7c7d947fdce840f0d2b418dd5e6
Paul DePalma <depalma@gonzaga.edu>
2017-10-05 09:29:50 -0700

    local variables

---
+++ ex29.cpp
@@ -0,0 +1,38 @@
+/*
+CPSC 121-0X
+Paul De Palma
+depalma
+Example 28 
+*/
+
+//local variables 
+
+#include <iostream>
+using namespace std;
+
+void shift(int,int);
+
+int main()
+{
+ int var1 = 10, var2 = 20;
+
+ cout << "variable var1 holds " << var1 << endl; 
+ cout << "variable vari2 holds " << var2 << endl; 
+
+ shift(var1,var2);
+
+ cout << "Returned from function" << endl;
+
+ cout << "variable var1 holds " << var1 << endl; 
+ cout << "variable vari2 holds " << var2 << endl; 
+ return 0;
+}
+
+void shift(int var1, int var2)
+{
+ int buf;
+
+ buf = var1;
+ var1 = var2;
+ var2 = buf;
+}
