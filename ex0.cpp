8280f2036a100c2e27bdc4537a9c8ea720c2ce62
Paul DePalma <depalma@gonzaga.edu>
2017-10-27 10:47:32 -0700

    new programs

---
+++ ex0.cpp
@@ -0,0 +1,18 @@
+/*
+CPSC 121-0X
+Paul De Palma
+depalma
+Example 0
+*/
+
+//A famous first program
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "Hello World" << endl;
+ return 0;
+}
